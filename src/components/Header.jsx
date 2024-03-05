3ba84f9f0e15719e786725da2e29934fb9c2ae43
rambriz91 <rambriz91@gmail.com>
2024-03-05 00:20:43 -0800

    created home page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 function Header() {
   return (
     <header className='flex'>
-      <div className='p-5 w-1/3'>
+      <div className='p-2 w-1/3 flex justify-center'>
         <img
           id='tsc-logo'
           src='/images/TSC Logo.png'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <div>
             <figure></figure>
+            <h2 className='m-5 p-5 text-center'>Today's Menu</h2>
         </div>
     )
 }
