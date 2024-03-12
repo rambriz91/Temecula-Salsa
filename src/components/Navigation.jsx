751b550c408f784f23355c0cb3685a805766dbb9
rambriz91 <rambriz91@gmail.com>
2024-03-12 09:42:20 -0700

    added hamburger nav element.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,20 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState('false');
+  const toggleMenu = () => {
+    setIsOpen((open) => !open)
+  }
+
   return (
     <nav id='navbar' className='w-3/4'>
-      <ul>
-        <li>
-          Home
-        </li>
-        <li>
-          Menu
-        </li>
-        <li>
-          Location
-        </li>
-        <li>
-          Contact Us
-        </li>
+      <i onClick={toggleMenu} id='hmbrgr' className='m-5 fa-solid fa-bars fa-2xl'></i>
+      <ul className={`nav-default flex justify-evenly items-center franklin ${isOpen ? 'is-open' : ''}`}>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Location</li>
+        <li>Contact Us</li>
       </ul>
     </nav>
   );
