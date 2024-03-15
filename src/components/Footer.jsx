52960bda17b56cc4209e33de7b59f5d6584693fa
rambriz91 <rambriz91@gmail.com>
2024-03-15 08:41:53 -0700

    linked footer elements to url.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
     <div>
         <h3 className='p-2 text-center'>Follow us on:</h3>
         <div className='flex justify-center'>
-            <img src="/images/icons8-facebook-96.png" alt="Facebook Logo" />
-            <img src="/images/icons8-instagram-96.png" alt="Instagram Logo" />
+            <a href="https://www.facebook.com/nancystamales"><img src="/images/icons8-facebook-96.png" alt="Facebook Logo" /></a>
+            <a href="https://www.instagram.com/temeculasalsaco/"><img src="/images/icons8-instagram-96.png" alt="Instagram Logo" /></a>
         </div>
         <div className='flex justify-center p-4'>
         <p className='franklin'>©️ 2024 Temecula Salsa Co All Rights Reserved</p>
